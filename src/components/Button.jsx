cd17984f362331a95da799c882adabed399ad689
Inseo Yang <y.1nseooo@gmail.com>
2023-03-13 13:28:28 +0900

    button component 수정

--- src/components/Button.jsx
+++ src/components/Button.jsx
@@ -1,7 +1,12 @@
 import React from "react";
 
 function Button() {
-  return <div>Button</div>;
+  return (
+    <div>
+      <h1>버튼 컴포넌트라고 했지만.. 버튼에 대한 내용은 없다..</h1>
+      <p>이거슨.. git 연습이기 때문이다...</p>
+    </div>
+  );
 }
 
 export default Button;
